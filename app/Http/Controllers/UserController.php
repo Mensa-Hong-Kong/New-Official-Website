1c9830f588c54943985dc6e26183651ab7cdcb77
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-09-16 01:09:18 +0800

    add routh path, controller and empty method

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class UserController extends Controller
+{
+    public function create()
+    {
+        // ...
+    }
+
+    public function store()
+    {
+        // ...
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'layouts.app')->name('index');
+Route::post('register', [UserController::class, 'create'])->name('authentication.register');
+Route::post('register', [UserController::class, 'store']);
