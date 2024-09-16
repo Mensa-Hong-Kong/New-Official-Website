fbc46b81f00ed6a636b749369c483ddffdc6211a
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-09-16 21:19:44 +0800

    rename register route

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'layouts.app')->name('index');
-Route::post('register', [UserController::class, 'create'])->name('authentication.register');
+Route::post('register', [UserController::class, 'create'])->name('register');
 Route::post('register', [UserController::class, 'store']);
