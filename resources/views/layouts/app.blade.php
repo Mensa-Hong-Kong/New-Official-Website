275234041173b5e098163f12c535908819308438
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-09-17 21:54:20 +0800

    add index and register link to index

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,31 +9,36 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="javascript:void(0)">Mensa</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="mynavbar">
-                <ul class="navbar-nav me-auto">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Dummy</a>
-                    </li>
-                </ul>
-                <hr class="my-3">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a href="#" @class([
-                            'nav-link',
-                            'align-items-center',
-                            'active' => Route::current()->getName() == 'register',
-                        ])>Register</a>
-                    </li>
-                </ul>
+    <header>
+        <nav class="navbar nav-pills navbar-expand-sm navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ route('index') }}">Mensa</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="mynavbar">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Dummy</a>
+                        </li>
+                    </ul>
+                    <hr class="my-3">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="{{ route('register') }}" @class([
+                                'nav-link',
+                                'align-items-center',
+                                'active' => Route::current()->getName() == 'register',
+                            ])>Register</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    </header>
+    <main style="height: 100%">
+        @yield('main')
+    </main>
 </body>
 
 </html>
