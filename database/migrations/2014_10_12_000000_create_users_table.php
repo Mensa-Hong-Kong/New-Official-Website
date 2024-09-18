f2dde57691a73ff589cd77a390d3a8bfb5dbe2a1
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-09-18 14:55:13 +0800

    fix user table same column missing nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,14 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique()->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('family_name')->nullable();
             $table->string('middle_name')->nullable();
             $table->string('given_name')->nullable();
             $table->unsignedBigInteger('gender_id');
             $table->unsignedBigInteger('passport_type_id');
-            $table->string('passport_number');
-            $table->date('birthdat');
+            $table->string('passport_number')->nullable();
+            $table->date('birthday');
             $table->rememberToken();
             $table->timestamps();
         });
