3e5338252947a9620cc32875222f22993f0b5766
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-09-21 01:36:41 +0800

    change the js to match Single responsibility principle

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,3 @@
 import './bootstrap';
 import bootstrap from '../../node_modules/bootstrap/js/index.umd';
 window.bootstrap = bootstrap;
-window.token = document.querySelector("meta[name='csrf-token']").getAttribute("content");
