1899e5144299bbc29461f12017e004b16b29c959
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-09-21 14:22:27 +0800

    change index test from url to route

--- tests/Feature/IndexTest.php
+++ tests/Feature/IndexTest.php
@@ -2,14 +2,13 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class IndexTest extends TestCase
 {
     public function testViewCanShow(): void
     {
-        $response = $this->get('/');
+        $response = $this->get(route('index'));
 
         $response->assertOk();
     }
