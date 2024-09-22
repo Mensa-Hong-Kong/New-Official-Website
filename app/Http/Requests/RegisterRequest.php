bd18548793284408564037df006ea5a0b71d3a25
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-09-22 21:21:22 +0800

    add custon message for passport tpye

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -25,7 +25,7 @@ class RegisterRequest extends FormRequest
             'given_name' => 'required|string|max:255',
             'passport_type_id' => 'required|integer|exists:'.PassportType::class.',id',
             'passport_number' => [
-                'required', 'string', 'min:8', 'max:18', 'regex:/^[A-Z0-9]+$/',
+                'required', 'regex:/^[A-Z0-9]+$/', 'min:8', 'max:18',
                 Rule::unique(User::class, 'passport_number')
                     ->where('passport_type_id', $this->passport_type_id)
             ],
@@ -33,6 +33,14 @@ class RegisterRequest extends FormRequest
             'birthday' => 'required|date|before_or_equal:'.now()->subYears(2)->format('Y-m-d'),
             'email' => 'nullable|email:rfc,dns',
             'mobile' => 'nullable|integer|min_digits:5|max_digits:15',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'passport_type_id.required' => 'The passport type field is required.',
+            'passport_type_id.exists' => 'The selected passport type is invalid.',
         ];
     }
 }
