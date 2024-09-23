74c843c95f0bde98f862139dd3ec21492a4e2afd
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-09-23 22:07:04 +0800

    try to fix mysql user exist

--- docker/mysql/schemas/999_user_permissions.sql
+++ docker/mysql/schemas/999_user_permissions.sql
@@ -1,3 +1,2 @@
-CREATE USER 'laravel'@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'password';
 GRANT USAGE ON *.* TO 'laravel'@'%';
 GRANT SELECT, INSERT, UPDATE, DELETE, DROP ON `mensa`.* TO 'laravel'@'%';
