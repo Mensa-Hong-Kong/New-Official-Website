9e2b1cb99366f8bb9c9a7a18e91ce480502f3b77
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-10-08 20:39:43 +0800

    fix missing save 2024_10_08_043308_create_contact_types_table

--- database/migrations/2024_10_08_043308_create_contact_types_table.php
+++ database/migrations/2024_10_08_043308_create_contact_types_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_contact_types', function (Blueprint $table) {
+        Schema::create('contact_types', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('url', 2048);
+            $table->boolean('can_verify');
             $table->timestamps();
         });
     }
@@ -22,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('user_contact_types');
+        Schema::dropIfExists('contact_types');
     }
 };
