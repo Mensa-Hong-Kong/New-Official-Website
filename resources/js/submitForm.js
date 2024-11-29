d3088b44dbc48d4ca18cbc6a11e6bb1afb3e570b
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-11-29 17:47:17 +0800

    fix 302 headle, fix fail callback and change alert to bootstrapAlert

--- resources/js/submitForm.js
+++ resources/js/submitForm.js
@@ -1,24 +1,23 @@
 import axios from 'axios';
 const token = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-export default function submitForm(action, method="post", data, successCallback = '', failCallback) {
+export default function submitForm(action, method="post", data, successCallback = '', failCallback = '') {
     data['_token'] = token;
     if(method != 'post') {
         data['_method'] = method;
     }
     axios.post(action, data).then(function (response) {
-        console.log(456);
+        if(response.status == 302) {
+            window.location.href = response.headers.location
+        }
         if(successCallback != '') {
             successCallback(response);
-        } else {
-            bootstrapAlert('Success');
         }
+        bootstrapAlert('Success');
     }).catch(function (error) {
         switch(error.status) {
-            case 302:
-                // header will auto redirect. need not to handle
-                break;
             case 401:
                 bootstrapAlert('Unauthorized, please login first');
+                window.location.href = `${windows.location.origin}/login`;
                 break;
             case 403:
                 bootstrapAlert('Sorry, you have no permission');
@@ -27,9 +26,7 @@ export default function submitForm(action, method="post", data, successCallback 
                 bootstrapAlert('Cross-site request forgery alert, may be the domain is not mensa.org.hk, or you hold on this page longer than the CSRF token lifetime');
                 break;
             case 422:
-                if(failCallback != '') {
-                    failCallback(error);
-                } else {
+                if(failCallback == '') {
                     bootstrapAlert('Some field type checking failed but I.T. have no set the handle, please check your typing or see GitHub what checking failed or contact I.T.');
                 }
                 break;
@@ -40,5 +37,6 @@ export default function submitForm(action, method="post", data, successCallback 
                 bootstrapAlert('Database connect fail, please try again later, or contact I.T.');
                 break;
         }
+        failCallback(error);
     });
 }
