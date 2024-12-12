f2cda361c4a24e19cc769b1ec3b76a7d8a3da59b
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-12-12 21:42:24 +0800

    add verification table and model

---
+++ app/Models/Verification.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Verification extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'verifiable_type',
+        'verifiable_id',
+        'tried_time',
+        'code',
+        'closed_at',
+        'verified_at',
+        'expired_at',
+    ];
+
+    protected $casts = [
+        'closed_at' => 'datetime',
+        'verified_at' => 'datetime',
+        'expired_at' => 'datetime',
+    ];
+}
