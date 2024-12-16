a5eb88c2dab753a7b5710b2f13069b3d37b6784c
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-12-16 14:28:26 +0800

    grouping and clearing contact controller codes

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Contact\VerifyRequest;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Support\Facades\DB;
 use Closure;
 
 class ContactController extends Controller implements HasMiddleware
@@ -21,42 +22,39 @@ class ContactController extends Controller implements HasMiddleware
                     if($contact->user_id != $user->id) {
                         abort(403);
                     }
+                    if($contact->isVerified()) {
+                        abort(410, "The {$contact->type} verified.");
+                    }
+                    return $next($request);
+                }, only: ['sendVerifyCode', 'verify']
+            ),
+            new Middleware(
+                function(Request $request, Closure $next) {
+                    $contact = $request->route('contact');
                     if($contact->isRequestTooFast()) {
-                        return response([
-                            'errors' => ['failed' => 'For each contact each minute only can get 1 time verify code, please again later.'],
-                        ], 429);
+                        abort(429, 'For each contact each minute only can get 1 time verify code, please again later.');
                     }
                     if($contact->isRequestTooManyTime()) {
-                        return response([
-                            'errors' => ['failed' => 'For each contact each day only can send 5 verify code, please again on tomorrow.'],
-                        ], 429);
+                        abort(429, 'For each contact each day only can send 5 verify code, please again on tomorrow.');
                     }
                     return $next($request);
                 }, only: ['sendVerifyCode']
             ),
             new Middleware(
                 function(Request $request, Closure $next) {
-                    $user = $request->user();
                     $contact = $request->route('contact');
-                    $error = '';
                     if(! $contact->lastVerification) {
                         $contact->sendVerifyCode();
-                        return response([
-                            'errors' => ['failed' => 'The verify request record is not found, the new verify code sent.'],
-                        ], 422);
+                        abort(404, 'The verify request record is not found, the new verify code sent.');
                     }
-                    if($contact->isVerified()) {
-                        return response([
-                            'success' => ['message' => 'The email verified.'],
-                        ], 200);
+                    $error = '';
+                    if($contact->lastVerification->isClosed()) {
+                        $error = 'The verify code expired';
                     }
-                    if($error == '') {
-                        if($contact->isClosed()) {
-                            $error = 'The verify code expired';
-                        }
-                        if($contact->lastVerification->isTriedTooManyTime()) {
-                            $error = 'The verify code tried more than 5 times';
-                        }
+                    if($contact->lastVerification->isTriedTooManyTime()) {
+                        $error = 'The verify code tried more than 5 times';
+                    }
+                    if($error != '') {
                         if($contact->isRequestTooManyTime()) {
                             $error .= ', this contact have sent 5 time verify code and each contact each day only can try 5 verify code, please again on tomorrow.';
                         } else {
@@ -76,12 +74,12 @@ class ContactController extends Controller implements HasMiddleware
     public function sendVerifyCode(Request $request, UserHasContact $contact)
     {
         $contact->sendVerifyCode();
-        return ['message' => 'The verify code sent!'];
+        return ['success' => 'The verify code sent!'];
     }
 
     public function verify(VerifyRequest $request, UserHasContact $contact)
     {
-        if($contact->code != strtoupper($request->code)) {
+        if($contact->lastVerification->code != strtoupper($request->code)) {
             $contact->lastVerification->increment('tried_time');
             $error = 'The verify code is incorrect';
             if($contact->lastVerification->isTriedTooManyTime()) {
@@ -92,17 +90,14 @@ class ContactController extends Controller implements HasMiddleware
                     $error .= ', the new verify code sent';
                     $contact->sendVerifyCode();
                 }
-                return response([
-                    'errors' => ['failed' => "$error."],
-                ], 422);
             }
             return response([
-                'errors' => ['failed' => 'The verify code is incorrect.'],
+                'errors' => ['failed' => "$error."],
             ], 422);
         }
         $contact->lastVerification->update(['verified_at' => now()]);
         return response([
-            'success' => ['message' => 'The email verifiy success.'],
+            'success' => "The {$contact->type} verifiy success.",
         ], 200);
     }
 }
