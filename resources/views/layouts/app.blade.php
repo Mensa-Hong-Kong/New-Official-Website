ef4c527cd612b88760371a0cc626ba5676bb7053
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-12-26 20:14:30 +0800

    takeout the "x" button to let can faster "tab" to cancel button

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,7 +65,6 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Alert</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <p id="alertMessage"></p>
