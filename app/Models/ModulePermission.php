e2905fb5e6e7e3a103f04828bf84fdbb1d34e4b9
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-12-31 18:05:40 +0800

    add model and add user model use HasRoles trait

---
+++ app/Models/ModulePermission.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Permission as Model;
+
+class ModulePermission extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'module_id',
+        'permission_id',
+        'guard_name',
+    ];
+}
---
+++ app/Models/TeamRole.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Permission\Models\Role as Model;
+
+class TeamRole extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'team_id',
+        'role_id',
+        'guard_name',
+    ];
+}
