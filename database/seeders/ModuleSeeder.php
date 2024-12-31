8b25b2d03b1897ff050f05bd461a1cfc584c238f
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2024-12-31 16:25:26 +0800

    takeout user info and user contact module

--- database/seeders/ModuleSeeder.php
+++ database/seeders/ModuleSeeder.php
@@ -12,25 +12,12 @@ class ModuleSeeder extends Seeder
     {
         $masterModule = Module::firstOrCreate(['name' => 'User']);
         $viewPermission = Permission::firstOrCreate(['name' => 'View']);
-        $masterModule->permissions()->sync([$viewPermission->id => ['name' => "{$viewPermission->name}:{$masterModule->name}"]]);
-        $module = Module::firstOrCreate(
-            [
-                'name' => 'Info',
-                'module_id' => $masterModule->id,
-            ]
-        );
         $editPermission = Permission::firstOrCreate(['name' => 'Edit']);
-        $module->permissions()->sync([$editPermission->id => ['name' => "{$editPermission->name}:{$module->name}"]]);
-        $module = Module::firstOrCreate(
-            [
-                'name' => 'Contact',
-                'module_id' => $masterModule->id,
-            ]
-        );
         $deletePermission = Permission::firstOrCreate(['name' => 'Delete']);
-        $module->permissions()->sync([
-            $editPermission->id => ['name' => "{$editPermission->name}:{$module->name}"],
-            $deletePermission->id => ['name' => "{$deletePermission->name}:{$module->name}"],
+        $masterModule->permissions()->sync([
+            $viewPermission->id => ['name' => "{$viewPermission->name}:{$masterModule->name}"]
+            $editPermission->id => ['name' => "{$editPermission->name}:{$masterModule->name}"]
+            $deletePermission->id => ['name' => "{$deletePermission->name}:{$masterModule->name}"]
         ]);
 
         app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
