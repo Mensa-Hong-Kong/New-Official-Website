98f2872a19a453b508f92fb611ff8963f6bb3752
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-01-11 18:58:51 +0800

    add update logic

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Permission;
+use Illuminate\Http\Request;
 
 class PermissionController extends Controller
 {
@@ -11,5 +12,13 @@ class PermissionController extends Controller
     {
         return view('admin.permission')
             ->with('permissions', Permission::orderBy('display_order')->get());
+    }
+
+    public function update(Request $request, Permission $permission)
+    {
+        if($request->name != $permission->title) {
+            $permission->update(['tit;e' => $request->name]);
+        }
+        return ['success' => 'The permission display name update success!'];
     }
 }
