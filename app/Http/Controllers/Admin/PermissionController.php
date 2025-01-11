628df615457b1db4eb349d058fe6fff6e7bca4b0
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-01-11 18:59:20 +0800

    add form validation

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\NameRequest;
 use App\Models\Permission;
-use Illuminate\Http\Request;
 
 class PermissionController extends Controller
 {
@@ -14,7 +14,7 @@ class PermissionController extends Controller
             ->with('permissions', Permission::orderBy('display_order')->get());
     }
 
-    public function update(Request $request, Permission $permission)
+    public function update(NameRequest $request, Permission $permission)
     {
         if($request->name != $permission->title) {
             $permission->update(['tit;e' => $request->name]);
