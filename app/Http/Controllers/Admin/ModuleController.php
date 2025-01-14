cd73f507f89b87633828cb7d7bd10d4c5f844e1d
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-01-14 14:41:00 +0800

    add displayOrder method to admin module controller

--- app/Http/Controllers/Admin/ModuleController.php
+++ app/Http/Controllers/Admin/ModuleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\NameRequest;
 use App\Models\Module;
+use Illuminate\Http\Request;
 
 class ModuleController extends Controller
 {
@@ -23,6 +24,22 @@ class ModuleController extends Controller
         return [
             'success' => 'The module display name update success!',
             'name' => $request->name,
+        ];
+    }
+
+    public function displayOrder(Request $request)
+    {
+        $case = [];
+        foreach(array_values($request->display_order) as $order => $id) {
+            $case[] = "WHEN id = $id THEN $order";
+        }
+        $case = implode(' ', $case);
+        Module::whereIn('id', $request->display_order)
+            ->update(['display_order' => "(CASE $case ELSE display_order END)"]);
+
+        return [
+            'success' => 'The display order update success!',
+            'display_order' => $request->display_order,
         ];
     }
 }
