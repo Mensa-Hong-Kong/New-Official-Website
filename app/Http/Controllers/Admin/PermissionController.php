4fa8cc9671d385a97c59f24c6070ab10c723aee8
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-01-14 21:49:10 +0800

    add displayOrder method to admin permission controller

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\NameRequest;
 use App\Models\Permission;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Support\Facades\DB;
 
 class PermissionController extends Controller implements HasMiddleware
 {
@@ -30,6 +32,25 @@ class PermissionController extends Controller implements HasMiddleware
         return [
             'success' => 'The permission display name update success!',
             'name' => $request->name,
+        ];
+    }
+
+    public function displayOrder(Request $request)
+    {
+        $case = [];
+        foreach (array_values($request->display_order) as $order => $id) {
+            $case[] = "WHEN id = $id THEN $order";
+        }
+        $case = implode(' ', $case);
+        Permission::whereIn('id', $request->display_order)
+            ->update(['display_order' => DB::raw("(CASE $case ELSE display_order END)")]);
+
+        return [
+            'success' => 'The display order update success!',
+            'display_order' => Permission::orderBy('display_order')
+                ->get('id')
+                ->pluck('id')
+                ->toArray(),
         ];
     }
 }
