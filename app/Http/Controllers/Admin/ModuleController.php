97b651824feb565276b01c2f44ce619b4834b900
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-01-15 16:25:17 +0800

    change response name field from request to model

--- app/Http/Controllers/Admin/ModuleController.php
+++ app/Http/Controllers/Admin/ModuleController.php
@@ -31,7 +31,7 @@ class ModuleController extends Controller implements HasMiddleware
 
         return [
             'success' => 'The module display name update success!',
-            'name' => $request->name,
+            'name' => $module->name,
         ];
     }
 
