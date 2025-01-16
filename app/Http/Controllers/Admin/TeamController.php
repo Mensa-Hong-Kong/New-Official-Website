37f9d6746f946f5205525ea333698b5e4ddf45b3
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-01-16 15:37:43 +0800

    add store method to admin team controller

--- app/Http/Controllers/Admin/TeamController.php
+++ app/Http/Controllers/Admin/TeamController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Team;
 use App\Models\TeamType;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TeamController extends Controller
 {
@@ -20,5 +23,21 @@ class TeamController extends Controller
                     ->orderBy('id')
                     ->get()
             );
+    }
+
+    public function store(Request $request)
+    {
+        DB::beginTransaction();
+        Team::where('type_id', $request->type_id)
+            ->where('display_order', '>=', $request->display_order)
+            ->increment('display_order');
+        Team::create([
+            'name' => $request->name,
+            'type_id' => $request->type_id,
+            'display_order' => $request->display_order,
+        ]);
+        DB::commit();
+
+        return redirect()->route('admin.teams.index');
     }
 }
