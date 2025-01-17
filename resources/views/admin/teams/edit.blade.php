ad3308792f0fdfa9687ba9b85f98efb05b5830a5
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-01-17 18:52:10 +0800

    fix missing update button id for copy create blade to edit blade

--- resources/views/admin/teams/edit.blade.php
+++ resources/views/admin/teams/edit.blade.php
@@ -6,8 +6,8 @@
             <h2 class="fw-bold mb-2 text-uppercase">Edit Team</h2>
             @method('put')
             @include('admin.teams.form')
-            <input type="submit" id="createButton" class="form-control btn btn-primary" value="Save">
-            <button class="form-control btn btn-primary" id="creatingButton" type="button" disabled hidden>
+            <input type="submit" id="saveButton" class="form-control btn btn-primary" value="Save">
+            <button class="form-control btn btn-primary" id="saveButton" type="button" disabled hidden>
                 <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
                 Saving...
             </button>
