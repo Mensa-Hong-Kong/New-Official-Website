339820efb13bd95d96ff2b1276ae480bf3b2b3fe
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-01-21 22:33:34 +0800

    add form view

--- resources/views/admin/teams/roles/create.blade.php
+++ resources/views/admin/teams/roles/create.blade.php
@@ -4,6 +4,7 @@
     <section class="container">
         <form id="form" method="POST" action="{{ route('admin.teams.roles.store', ['team' => $team]) }}" novalidate>
             <h2 class="fw-bold mb-2 text-uppercase">Create Role For {{ $team->name }}</h2>
+            @include('admin.teams.roles.form')
             <input type="submit" id="createButton" class="form-control btn btn-success" value="Create">
             <button class="form-control btn btn-success" id="creatingButton" type="button" disabled hidden>
                 <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
@@ -12,3 +13,4 @@
         </form>
     </section>
 @endsection
+
