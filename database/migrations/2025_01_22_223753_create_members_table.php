bcc282ad1b476686947ce08904856441fb4f8ddf
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-01-22 22:53:45 +0800

    fix before PR missing save members table migration

--- database/migrations/2025_01_22_223753_create_members_table.php
+++ database/migrations/2025_01_22_223753_create_members_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('members', function (Blueprint $table) {
-            $table->id();
+            $table->unsignedBigInteger('id');
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
