2c49c1867d9ed605e46b8a66a99375c43c91adbb
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-01-23 16:51:15 +0800

    add admin admission test controller and store method

---
+++ app/Http/Controllers/Admin/AdmissionTestController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Address;
+use App\Models\AdmissionTest;
+use App\Models\Location;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AdmissionTestController extends Controller
+{
+    public function store(Request $request)
+    {
+        DB::beginTransaction();
+        $address = Address::firstOrCreate([
+            'district_id' => $request->district_id,
+            'address' => $request->address,
+        ]);
+        $location = Location::firstOrCreate([
+            'address_id' => $address->id,
+            'name' => $request->location,
+        ]);
+        AdmissionTest::create([
+            'testing_at' => $request->testing_at,
+            'location_id' => $location->id,
+            'maximum_candidates' => $request->maximum_candidates,
+            'is_public' => $request->is_public,
+        ]);
+        DB::commit();
+
+        return redirect()->route('admin.index');
+    }
+}
