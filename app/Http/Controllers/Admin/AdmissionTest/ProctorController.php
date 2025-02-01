16d849da1123083077120504093f7182a1ed6afd
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-02-01 21:32:48 +0800

    admin proctor controller add update method

--- app/Http/Controllers/Admin/AdmissionTest/ProctorController.php
+++ app/Http/Controllers/Admin/AdmissionTest/ProctorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\AdmissionTest;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\AdmissionTest\ProctorRequest;
 use App\Models\AdmissionTest;
+use App\Models\AdmissionTestHasProctor;
 use App\Models\User;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
@@ -36,6 +37,36 @@ class ProctorController extends Controller implements HasMiddleware
             'show_user_url' => route(
                 'admin.users.show',
                 ['user' => $user]
+            ),
+        ];
+    }
+
+    public function update(ProctorRequest $request, AdmissionTest $admissionTest, User $proctor)
+    {
+        $user = User::find($request->user_id);
+        if (! $user) {
+            return response([
+                'errors' => ['user_id' => 'The selected user id is invalid.'],
+            ], 422);
+        }
+        AdmissionTestHasProctor::where('test_id', $admissionTest->id)
+            ->where('user_id', $proctor->id)
+            ->update(['user_id' => $user->id]);
+
+        return [
+            'success' => 'Update proctor success',
+            'user_id' => $user->id,
+            'name' => $user->name,
+            'show_user_url' => route(
+                'admin.users.show',
+                ['user' => $user]
+            ),
+            'update_proctor_url' => route(
+                'admin.admission-tests.proctors.update',
+                [
+                    'admission_test' => $admissionTest,
+                    'proctor' => $user,
+                ]
             ),
         ];
     }
