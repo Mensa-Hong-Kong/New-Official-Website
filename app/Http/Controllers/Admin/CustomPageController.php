785fda9b3fbed2bbd7b65e2d8e94e0e708a4feca
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-02-03 19:03:46 +0800

    add permission middleware

--- app/Http/Controllers/Admin/CustomPageController.php
+++ app/Http/Controllers/Admin/CustomPageController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\CustomPage;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class CustomPageController extends Controller
+class CustomPageController extends Controller implements HasMiddleware
 {
+    public static function middleware(): array
+    {
+        return [(new Middleware('permission:Edit:Custom Page'))->except('index')];
+    }
+
     public function store(Request $request)
     {
         $pathname = preg_replace('/\/+/', '/', $request->pathname);
