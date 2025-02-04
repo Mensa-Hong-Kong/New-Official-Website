c4e61573c218787ba26d88188fde5a0ce8e5e444
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-02-04 18:46:20 +0800

    add form

--- resources/views/admin/custom-pages/create.blade.php
+++ resources/views/admin/custom-pages/create.blade.php
@@ -4,6 +4,7 @@
     <section class="container">
         <form id="form" method="POST" action="{{ route('admin.custom-pages.store') }}" novalidate>
             <h2 class="fw-bold mb-2 text-uppercase">Create Custom Page</h2>
+            @include('admin.custom-pages.form')
             <input type="submit" id="createButton" class="form-control btn btn-success" value="Create">
             <button class="form-control btn btn-success" id="creatingButton" type="button" disabled hidden>
                 <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
