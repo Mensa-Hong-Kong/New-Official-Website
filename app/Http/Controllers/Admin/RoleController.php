d66c51d7c3dd3161f274ec6bdf5c289c112d2164
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-02-08 15:00:10 +0800

    fix role missing update display order logic

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -127,6 +127,21 @@ class RoleController extends Controller implements HasMiddleware
         $teamRole = TeamRole::where('team_id', $team->id)
             ->where('role_id', $role->id)
             ->first();
+        if($teamRole->display_order > $request->display_order) {
+            TeamRole::where('team_id', $team->id)
+                ->where('display_order', '>', $teamRole->display_order)
+                ->decrement('display_order');
+            TeamRole::where('team_id', $team->id)
+                ->where('display_order', '>=', $request->display_order)
+                ->increment('display_order');
+        } else {
+            TeamRole::where('team_id', $team->id)
+                ->where('display_order', '>=', $request->display_order)
+                ->increment('display_order');
+            TeamRole::where('team_id', $team->id)
+                ->where('display_order', '>', $teamRole->display_order)
+                ->decrement('display_order');
+        }
         $teamRole->update([
             'name' => "{$team->type->name}:{$team->name}:{$role->name}",
             'display_order' => $request->display_order,
