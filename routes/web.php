5755b3adaa11b86dcb39c1f6f88b24c9b827c627
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-02-08 18:04:27 +0800

    update route for edit nav item

--- routes/web.php
+++ routes/web.php
@@ -117,7 +117,7 @@ Route::middleware('auth')->group(function () {
                 ->except('show')
                 ->whereNumber('custom_page');
             Route::resource('navigation-items', AdmissionNavigationItemController::class)
-                ->except(['show', 'edit', 'destroy'])
+                ->except(['show', 'destroy'])
                 ->whereNumber('navigation_item');
             Route::match(['put', 'patch'], 'navigation-items/display-order', [AdmissionNavigationItemController::class, 'displayOrder'])
                 ->name('navigation-items.display-order.update');
