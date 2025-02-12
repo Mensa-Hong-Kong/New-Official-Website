c0da8f058d69213bba00e29f73e68cedb989000a
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-02-12 23:39:07 +0800

    add middleware

--- app/Http/Controllers/Admin/AdmissionTest/CandidateController.php
+++ app/Http/Controllers/Admin/AdmissionTest/CandidateController.php
@@ -6,11 +6,30 @@ use App\Http\Controllers\Controller;
 use App\Models\AdmissionTest;
 use App\Models\AdmissionTestHasCandidate;
 use App\Models\User;
+use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\DB;
 
-class CandidateController extends Controller
+class CandidateController extends Controller implements HasMiddleware
 {
+    public static function middleware(): array
+    {
+        return [
+            new Middleware('permission:Edit:Admission Test'),
+            new Middleware('permission:View:User'),
+            (new Middleware(
+                function (Request $request, Closure $next) {
+                    if($request->route('admission_test')->expect_end_at->addHour() < now()) {
+                        abort(410, 'Can not change candidate after than expect end time one hour.');
+                    }
+                    return $next($request);
+                }
+            )),
+        ];
+    }
+
     public function store(Request $request, AdmissionTest $admissionTest)
     {
         DB::beginTransaction();
