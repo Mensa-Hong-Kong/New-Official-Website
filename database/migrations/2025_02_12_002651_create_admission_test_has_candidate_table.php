5a4e800a6ef01b5673fd4def7608523fd5c4bd6f
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-02-12 23:05:22 +0800

    add admission_test_has_candidate table

---
+++ database/migrations/2025_02_12_002651_create_admission_test_has_candidate_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admission_test_has_candidate', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('test_id');
+            $table->unsignedBigInteger('user_id');
+            $table->boolean('is_present')->default(false);
+            $table->boolean('is_pass')->nullable();
+            $table->unsignedBigInteger('passport_type_id');
+            $table->string('passport_number')->nullable();
+            $table->timestamps();
+            $table->foreign('test_id')
+                ->references('id') // admission test id
+                ->on('admission_tests')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admission_test_has_candidate');
+    }
+};
