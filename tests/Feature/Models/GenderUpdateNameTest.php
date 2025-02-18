6ddb3e8723085a051c82f4a10f0dcd1f12b48330
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-02-18 22:01:14 +0800

    fix missing up date class name

--- tests/Feature/Models/GenderUpdateNameTest.php
+++ tests/Feature/Models/GenderUpdateNameTest.php
@@ -7,7 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class GenderUpdateTest extends TestCase
+class GenderUpdateNameTest extends TestCase
 {
     use RefreshDatabase;
 
