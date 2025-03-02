ff008ac2c3ee311dab2cc90385b96963fed765dc
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-03-02 22:02:31 +0800

    fix coding style

--- app/Http/Controllers/AdmissionTestController.php
+++ app/Http/Controllers/AdmissionTestController.php
@@ -10,17 +10,17 @@ class AdmissionTestController extends Controller
     {
         return view('admission-tests.index')
             ->with(
-                'tests', AdmissionTest::where('testing_at', ">=", now())
+                'tests', AdmissionTest::where('testing_at', '>=', now())
                     ->where(
-                        function($query) {
+                        function ($query) {
                             $query->where('is_public', true);
                             $user = auth()->user();
-                            if($user) {
+                            if ($user) {
                                 $query->orWhereHas(
-                                        'candidates', function($query) {
-                                            $query->where('user_id', auth()->user()->id);
-                                        }
-                                    );
+                                    'candidates', function ($query) {
+                                        $query->where('user_id', auth()->user()->id);
+                                    }
+                                );
                             }
                         }
                     )->orderBy('testing_at')
