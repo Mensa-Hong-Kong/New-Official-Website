07c462b60b70bd18e512a1b1cbd84b96191f4811
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-03-07 00:40:35 +0800

    admin site page controller add update method

--- app/Http/Controllers/Admin/SitePageController.php
+++ app/Http/Controllers/Admin/SitePageController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\SiteContent;
 use App\Models\SitePage;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
 
@@ -18,5 +20,12 @@ class SitePageController extends Controller implements HasMiddleware
     {
         return view('admin.site-contents.index')
             ->with('pages', SitePage::all());
+    }
+
+    public function update(Request $request, SiteContent $siteContent)
+    {
+        $siteContent->update(['content' => $request->content]);
+
+        return redirect()->route('admin.site-contents.index');
     }
 }
