d75240f1b0aedb8e615d67df2393b51c11d9e307
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-03-07 00:01:57 +0800

    add permission middleware

--- app/Http/Controllers/Admin/SitePageController.php
+++ app/Http/Controllers/Admin/SitePageController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\SitePage;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class SitePageController extends Controller
+class SitePageController extends Controller implements HasMiddleware
 {
+    public static function middleware(): array
+    {
+        return [(new Middleware('permission:Edit:Site Content'))];
+    }
+
     public function index()
     {
         return view('admin.site-contents.index')
