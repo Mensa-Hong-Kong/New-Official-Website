cfb0781ae4ac32dc6216bf23cc35b290d575cb16
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-03-08 20:52:00 +0800

    add candidate controller and store method

---
+++ app/Http/Controllers/CandidateController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AdmissionTest;
+use App\Notifications\AdmissionTest\RescheduleAdmissionTest;
+use App\Notifications\AdmissionTest\ScheduleAdmissionTest;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\DB;
+
+class CandidateController extends Controller
+{
+    public function store(Request $request, AdmissionTest $admissionTest)
+    {
+        $user = $request->user();
+        DB::beginTransaction();
+        $admissionTest->candidates()->attach($user->id);
+        switch ($request->function) {
+            case 'schedule':
+                $user->notify(new ScheduleAdmissionTest($admissionTest));
+                break;
+            case 'reschedule':
+                $oldTest = AdmissionTest::whereNot('id', $admissionTest->id)
+                    ->where('testing_at', '>', now())
+                    ->first();
+                $oldTestForDelete = clone $oldTest;
+                $oldTestForDelete->delete();
+                $user->notify(new RescheduleAdmissionTest($oldTest, $admissionTest));
+                break;
+        }
+        DB::commit();
+
+        return redirect()->route('admission-tests.index');
+    }
+}
