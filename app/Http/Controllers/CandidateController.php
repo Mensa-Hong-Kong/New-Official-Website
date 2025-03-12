6a6b5051f36e464d7279b9fe98c905f1af6d9c70
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-03-12 22:33:22 +0800

    remove unused form validation

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -2,34 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CandidateRequest;
 use App\Models\AdmissionTest;
 use App\Notifications\AdmissionTest\RescheduleAdmissionTest;
 use App\Notifications\AdmissionTest\ScheduleAdmissionTest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class CandidateController extends Controller
 {
-    public function store(CandidateRequest $request, AdmissionTest $admissionTest)
+    public function store(Request $request, AdmissionTest $admissionTest)
     {
         $user = $request->user();
         DB::beginTransaction();
         $admissionTest->candidates()->attach($user->id);
-        switch ($request->function) {
-            case 'schedule':
-                $user->notify(new ScheduleAdmissionTest($admissionTest));
-                break;
-            case 'reschedule':
-                $oldTest = AdmissionTest::whereNot('id', $admissionTest->id)
-                    ->where('testing_at', '>', now())
-                    ->first();
-                $oldTestForDelete = clone $oldTest;
-                $oldTestForDelete->delete();
-                $user->notify(new RescheduleAdmissionTest($oldTest, $admissionTest));
-                break;
+        if($request->futureTest) {
+            $oldTest = clone $request->futureTest;
+            $oldTest->delete();
+            $user->notify(new RescheduleAdmissionTest($request->futureTest, $admissionTest));
+            $success = 'Your reschedule request successfully, the new ticket will be to your default contact(s).';
+        } else {
+            $user->notify(new ScheduleAdmissionTest($admissionTest));
+            $success = 'Your schedule request successfully, the ticket will be to your default contact(s).';
         }
         DB::commit();
 
-        return redirect()->route('admission-tests.index');
+        return redirect()->route('admission-tests.index')
+            ->with('success', $success);
     }
 }
