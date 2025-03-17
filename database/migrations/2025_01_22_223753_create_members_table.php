9847ce6f62a8e09b0c977b44a5f092fe0720c869
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-03-17 20:03:32 +0800

    update members table structure and update member model

--- database/migrations/2025_01_22_223753_create_members_table.php
+++ database/migrations/2025_01_22_223753_create_members_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -14,6 +15,13 @@ return new class extends Migration
         Schema::create('members', function (Blueprint $table) {
             $table->unsignedBigInteger('id');
             $table->unsignedBigInteger('user_id');
+            $table->boolean('is_active')->default(false);
+            $table->date('expired_on');
+            $table->date('actual_expired_on');
+            $table->string('prefix_name')->nullable();
+            $table->string('nickname')->nullable();
+            $table->string('suffix_name')->nullable();
+            $table->string('address_id')->nullable();
             $table->timestamps();
         });
     }
