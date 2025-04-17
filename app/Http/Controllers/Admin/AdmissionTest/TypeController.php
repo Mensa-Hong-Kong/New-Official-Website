34d4e7a276298134686b892aac25c0388c871c09
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-04-17 13:36:37 +0800

    add middleware

--- app/Http/Controllers/Admin/AdmissionTest/TypeController.php
+++ app/Http/Controllers/Admin/AdmissionTest/TypeController.php
@@ -5,9 +5,16 @@ namespace App\Http\Controllers\Admin\AdmissionTest;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\AdmissionTest\TypeRequest;
 use App\Models\AdmissionTestType;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class TypeController extends Controller
+class TypeController extends Controller implements HasMiddleware
 {
+    public static function middleware(): array
+    {
+        return [(new Middleware('permission:Edit:Admission Test'))];
+    }
+
     public function store(TypeRequest $request)
     {
         AdmissionTestType::create([
