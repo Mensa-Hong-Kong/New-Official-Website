233c1013ccf4528d3c8ea83c12feb21811bf2d6e
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-04-17 13:37:10 +0800

    add factory

---
+++ database/factories/AdmissionTestTypeFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\District;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class AdmissionTestTypeFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word(),
+            'interval_month' => fake()->numberBetween(0, 60),
+            'is_active' => fake()->randomElement([true, false]),
+            'display_order' => fake()->randomElement([true, false]),
+        ];
+    }
+}
