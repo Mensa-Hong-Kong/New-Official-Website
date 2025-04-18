2bab15be4e0201ad9b50832b288e85b5ba815af2
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-04-18 11:37:02 +0800

    add edit method into controller

--- app/Http/Controllers/Admin/AdmissionTest/TypeController.php
+++ app/Http/Controllers/Admin/AdmissionTest/TypeController.php
@@ -56,6 +56,27 @@ class TypeController extends Controller implements HasMiddleware
         return redirect()->route('admin.admission-test-types.index');
     }
 
+    public function edit(AdmissionTestType $admissionTestType)
+    {
+        $types = AdmissionTestType::orderBy('display_order')
+            ->get(['name', 'display_order'])
+            ->pluck('name', 'display_order')
+            ->toArray();
+        foreach ($types as $displayOrder => $name) {
+            $types[$displayOrder] = "before \"$name\"";
+        }
+        if ($admissionTestType->display_order == max(array_keys($types))) {
+            $types[max(array_keys($types))] = 'latest';
+        } else {
+            $types[max(array_keys($types)) + 1] = 'latest';
+        }
+        $types[0] = 'top';
+
+        return view('admin.admission-test-types.edit')
+            ->with('type', $admissionTestType)
+            ->with('types', $types);
+    }
+
     public function update(TypeRequest $request, AdmissionTestType $admissionTestType)
     {
         DB::beginTransaction();
