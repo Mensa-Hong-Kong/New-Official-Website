35fe0f68babf952f239d557300e93c8d733eeca2
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-04-22 23:06:45 +0800

    add auto change synced_to_stripe status to false when price name changed

--- app/Models/AdmissionTestPrice.php
+++ app/Models/AdmissionTestPrice.php
@@ -18,6 +18,20 @@ class AdmissionTestPrice extends Model
         'synced_to_stripe',
     ];
 
+    /**
+     * The "booted" method of the model.
+     */
+    protected static function booted(): void
+    {
+        static::updating(
+            function (AdmissionTestPrice $price) {
+                if ($price->isDirty('name')) {
+                    $price->synced_to_stripe = false;
+                }
+            }
+        );
+    }
+
     public function product()
     {
         return $this->belongsTo(AdmissionTestProduct::class, 'product_id');
