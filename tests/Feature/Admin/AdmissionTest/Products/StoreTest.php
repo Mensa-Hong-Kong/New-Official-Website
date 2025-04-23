97d7dae8a37d0db30042589640ceff719c7c7207
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-04-23 20:04:34 +0800

    update test for product add fields

--- tests/Feature/Admin/AdmissionTest/Products/StoreTest.php
+++ tests/Feature/Admin/AdmissionTest/Products/StoreTest.php
@@ -14,9 +14,8 @@ class StoreTest extends TestCase
     private $user;
 
     private $happyCase = [
-        'name' => 'Admission Test - Team',
-        'minimum_age' => 14,
-        'maximum_age' => 22,
+        'name' => 'Admission Test',
+        'quota' => 1,
     ];
 
     protected function setUp(): void
@@ -115,14 +114,15 @@ class StoreTest extends TestCase
     public function test_minimum_age_greater_than_maximum_age()
     {
         $data = $this->happyCase;
+        $data['minimum_age'] = 14;
         $data['maximum_age'] = 13;
         $response = $this->actingAs($this->user)->postJson(
             route('admin.admission-test.products.store'),
             $data
         );
         $response->assertInvalid([
-            'minimum_age' => 'The minimum age field must be less than maximum age.',
-            'maximum_age' => 'The maximum age field must be greater than minimum age.',
+            'minimum_age' => 'The minimum age field must be less than maximum age field.',
+            'maximum_age' => 'The maximum age field must be greater than minimum age field.',
         ]);
     }
 
@@ -159,11 +159,196 @@ class StoreTest extends TestCase
         $response->assertInvalid(['maximum_age' => 'The maximum age field must not be greater than 255.']);
     }
 
-    public function test_happy_case()
+    public function test_start_at_is_not_date()
+    {
+        $data = $this->happyCase;
+        $data['start_at'] = 'abc';
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertInvalid(['start_at' => 'The start at field must be a valid date.']);
+    }
+
+    public function test_start_at_after_than_end_at()
+    {
+        $now = now();
+        $data = $this->happyCase;
+        $data['end_at'] = $now;
+        $data['start_at'] = $now->addHour();
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertInvalid([
+            'start_at' => 'The start at field must be a date before end at field.',
+            'end_at' => 'The end at field must be a date after start at field.',
+        ]);
+    }
+
+    public function test_end_at_is_not_date()
+    {
+        $data = $this->happyCase;
+        $data['end_at'] = 'abc';
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertInvalid(['end_at' => 'The end at field must be a valid date.']);
+    }
+
+    public function test_missing_quota()
+    {
+        $data = $this->happyCase;
+        unset($data['quota']);
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertInvalid(['quota' => 'The quota field is required.']);
+    }
+
+    public function test_quota_is_not_integer()
+    {
+        $data = $this->happyCase;
+        $data['quota'] = 'abc';
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertInvalid(['quota' => 'The quota field must be an integer.']);
+    }
+
+    public function test_quota_less_than_1()
+    {
+        $data = $this->happyCase;
+        $data['quota'] = -1;
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertInvalid(['quota' => 'The quota field must be at least 1.']);
+    }
+
+    public function test_quota_greater_than_255()
+    {
+        $data = $this->happyCase;
+        $data['quota'] = 256;
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertInvalid(['quota' => 'The quota field must not be greater than 255.']);
+    }
+
+    public function test_happy_case_without_all_nullable_field()
     {
         $response = $this->actingAs($this->user)->postJson(
             route('admin.admission-test.products.store'),
             $this->happyCase
+        );
+        $response->assertRedirectToRoute(
+            'admin.admission-test.products.show',
+            ['product' => AdmissionTestProduct::latest('id')->first()]
+        );
+    }
+
+    public function test_happy_case_with_minimum_age()
+    {
+        $data = $this->happyCase;
+        $data['minimum_age'] = 14;
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertRedirectToRoute(
+            'admin.admission-test.products.show',
+            ['product' => AdmissionTestProduct::latest('id')->first()]
+        );
+    }
+
+    public function test_happy_case_with_maximum_age()
+    {
+        $data = $this->happyCase;
+        $data['maximum_age'] = 22;
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertRedirectToRoute(
+            'admin.admission-test.products.show',
+            ['product' => AdmissionTestProduct::latest('id')->first()]
+        );
+    }
+
+    public function test_happy_case_with_minimum_and_maximum_age()
+    {
+        $data = $this->happyCase;
+        $data['maximum_age'] = 14;
+        $data['maximum_age'] = 22;
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertRedirectToRoute(
+            'admin.admission-test.products.show',
+            ['product' => AdmissionTestProduct::latest('id')->first()]
+        );
+    }
+
+    public function test_happy_case_with_start_at()
+    {
+        $data = $this->happyCase;
+        $data['start_at'] = now();
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertRedirectToRoute(
+            'admin.admission-test.products.show',
+            ['product' => AdmissionTestProduct::latest('id')->first()]
+        );
+    }
+
+    public function test_happy_case_with_end_at()
+    {
+        $data = $this->happyCase;
+        $data['end_at'] = now();
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertRedirectToRoute(
+            'admin.admission-test.products.show',
+            ['product' => AdmissionTestProduct::latest('id')->first()]
+        );
+    }
+
+    public function test_happy_case_with_start_ans_end_at()
+    {
+        $data = $this->happyCase;
+        $data['start_at'] = now();
+        $data['end_at'] = now()->addDay();
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
+        );
+        $response->assertRedirectToRoute(
+            'admin.admission-test.products.show',
+            ['product' => AdmissionTestProduct::latest('id')->first()]
+        );
+    }
+
+    public function test_happy_case_with_all_nullable_field()
+    {
+        $data = $this->happyCase;
+        $data['maximum_age'] = 14;
+        $data['maximum_age'] = 22;
+        $data['start_at'] = now();
+        $data['end_at'] = now()->addDay();
+        $response = $this->actingAs($this->user)->postJson(
+            route('admin.admission-test.products.store'),
+            $data
         );
         $response->assertRedirectToRoute(
             'admin.admission-test.products.show',
