673a3fb3ac802beadfed77c2fc0d2bd0f73fbae5
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-05-31 17:27:26 +0800

    fix is_active column missing defauld false

--- database/migrations/2025_05_27_211341_create_other_payment_gateways_table.php
+++ database/migrations/2025_05_27_211341_create_other_payment_gateways_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('other_payment_gateways', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('is_active');
+            $table->boolean('is_active')->default(0);
             $table->unsignedBigInteger('display_order')->default(0);
             $table->timestamps();
         });
