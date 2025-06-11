d867df6a6c0fa712d6bc93e6570a636d6d81db8d
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-06-11 11:39:49 +0800

    rename route profile.created-stripe-user to profile.created-stripe-customer

--- resources/js/components/stripeAlert.js
+++ resources/js/components/stripeAlert.js
@@ -1,10 +1,14 @@
 function checkSyncedToStripe() {
-    axios.get('/profile/created-stripe-user').then(function (response) {
-        if(response.data.status) {
-            document.getElementById('stripeCustomerNotUpToDateAlert').remove();
-            clearInterval(interval);
+    axios.get(
+        route('profile.created-stripe-customer')
+    ).then(
+        function (response) {
+            if(response.data.status) {
+                document.getElementById('stripeCustomerNotUpToDateAlert').remove();
+                clearInterval(interval);
+            }
         }
-    });
+    );
 }
 
 let interval = setInterval(checkSyncedToStripe, 30000);
--- routes/web.php
+++ routes/web.php
@@ -54,8 +54,8 @@ Route::any('logout', [UserController::class, 'logout'])->name('logout');
 Route::middleware('auth')->group(function () {
     Route::singleton('profile', UserController::class)
         ->except('edit');
-    Route::get('profile/created-stripe-user', [UserController::class, 'createdStripeUser'])
-        ->name('profile.created-stripe-user');
+    Route::get('profile/created-stripe-customer', [UserController::class, 'createdStripeCustomer'])
+        ->name('profile.created-stripe-customer');
     Route::get('contacts/{contact}/send-verify-code', [ContactController::class, 'sendVerifyCode'])
         ->name('contacts.send-verify-code')
         ->whereNumber('contact');
