83b805f194e2b278c278fca49e16b7decaba3310
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-06-15 12:53:25 +0800

    change clearInputHistory.js to support  svelte

--- resources/js/clearInputHistory.js
+++ resources/js/clearInputHistory.js
@@ -1,22 +1,28 @@
 export default class ClearInputHistory
 {
-    constructor(inputs) {
-        window.addEventListener(
-            "pagehide", function() {
-                for(let input of inputs) {
-                    if(input.type == 'date') {
-                        if(input.max) {
-                            input.value = input.max;
-                        } else if(input.min) {
-                            input.value = input.max;
-                        } else {
-                            input.value = '';
-                        }
-                    } else {
-                        input.value = '';
-                    }
+    invoke(event) {
+        for(let input of this.inputs) {
+            if(input.type == 'date') {
+                if(input.max) {
+                    input.value = input.max;
+                } else if(input.min) {
+                    input.value = input.min;
+                } else {
+                    input.value = '';
                 }
+            } else {
+                input.value = '';
             }
-        );
+        }
+    }
+
+    constructor(inputs) {
+        this.inputs = inputs;
+        this.invoke = this.invoke.bind(this)
+        window.addEventListener("pagehide", this.invoke);
+    }
+
+    destroy() {
+        window.removeEventListener("pagehide", this.invoke);
     }
 }
