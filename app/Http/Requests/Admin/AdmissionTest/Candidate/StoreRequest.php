651e961059008bd42c671b024ebdb1093c4eef6e
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-09-19 19:38:46 +0800

    change candidaye proctor and candidate form validation from using after method to using closure inside rules method response

--- app/Http/Requests/Admin/AdmissionTest/Candidate/StoreRequest.php
+++ app/Http/Requests/Admin/AdmissionTest/Candidate/StoreRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\Admin\AdmissionTest\Candidate;
 
 use App\Models\AdmissionTestHasCandidate;
 use App\Models\User;
-use Illuminate\Contracts\Validation\Validator;
+use Closure;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
@@ -19,9 +19,45 @@ class StoreRequest extends FormRequest
     {
         $unique = Rule::unique(AdmissionTestHasCandidate::class)
             ->where('test_id', $this->route('admission_test'));
+        $request = $this;
 
         return [
-            'user_id' => ['required', 'integer', $unique],
+            'user_id' => [
+                'required', 'integer', $unique,
+                function (string $attribute, mixed $value, Closure $fail) use ($request) {
+                    $request->merge(['user' => User::find($value)]);
+                    if (! $request->user) {
+                        $fail('The selected user id is invalid.');
+                    } elseif ($request->user->isActiveMember()) {
+                        $fail('The selected user id has already member.');
+                    } elseif ($request->user->hasQualificationOfMembership) {
+                        $fail('The selected user id has already qualification for membership.');
+                    } elseif (
+                        $request->user->futureAdmissionTest && 
+                        $request->user->futureAdmissionTest->id == $request->route('admission_test')->id
+                    ) {
+                        $fail('The selected user id has already schedule this admission test.');
+                    } elseif ($request->function == 'schedule' && $request->user->futureAdmissionTest) {
+                        $fail('The selected user id has already schedule other admission test.');
+                    } elseif ($request->function == 'reschedule' && ! $request->user->futureAdmissionTest) {
+                        $fail('The selected user id have no scheduled other admission test after than now.');
+                    } elseif ($request->user->hasSamePassportAlreadyQualificationOfMembership) {
+                        $fail('The passport of selected user id has already been qualification for membership.');
+                    } elseif ($request->user->lastAttendedAdmissionTestOfOtherSamePassportUser) {
+                        $fail('The selected user id has other same passport user account tested.');
+                    } elseif (
+                        $request->user->lastAttendedAdmissionTest &&
+                        $request->user->lastAttendedAdmissionTest->testing_at
+                            ->addMonths(
+                                $request->user->lastAttendedAdmissionTest->type->interval_month
+                            )->endOfDay() >= $request->route('admission_test')->testing_at
+                    ) {
+                        $fail("The selected user id has admission test record within {$request->user->lastAttendedAdmissionTest->type->interval_month} months(count from testing at of this test sub {$request->user->lastAttendedAdmissionTest->type->interval_month} months to now).");
+                    } elseif (! $request->user->defaultEmail && ! $request->user->defaultMobile) {
+                        $fail('The selected user must at least has default contact.');
+                    }
+                }
+            ],
             'function' => 'required|string|in:schedule,reschedule',
         ];
     }
@@ -29,79 +65,5 @@ class StoreRequest extends FormRequest
     public function messages(): array
     {
         return ['function.in' => 'The function field does not exist in schedule, reschedule.'];
-    }
-
-    public function after()
-    {
-        return [
-            function (Validator $validator) {
-                $user = User::find($this->user_id);
-                if (! $user) {
-                    $validator->errors()->add(
-                        'user_id',
-                        'The selected user id is invalid.'
-                    );
-                } else {
-                    $now = now();
-                    $admissionTest = $this->route('admission_test');
-                    $this->merge([
-                        'user' => $user,
-                        'now' => $now,
-                    ]);
-                    if ($this->user->isActiveMember()) {
-                        $validator->errors()->add(
-                            'user_id',
-                            'The selected user id has already member.'
-                        );
-                    } elseif ($this->user->hasQualificationOfMembership) {
-                        $validator->errors()->add(
-                            'user_id',
-                            'The selected user id has already qualification for membership.'
-                        );
-                    } elseif ($this->user->futureAdmissionTest && $user->futureAdmissionTest->id == $admissionTest->id) {
-                        $validator->errors()->add(
-                            'user_id',
-                            'The selected user id has already schedule this admission test.'
-                        );
-                    } elseif ($this->function == 'schedule' && $user->futureAdmissionTest) {
-                        $validator->errors()->add(
-                            'user_id',
-                            'The selected user id has already schedule other admission test.'
-                        );
-                    } elseif ($this->function == 'reschedule' && ! $user->futureAdmissionTest) {
-                        $validator->errors()->add(
-                            'user_id',
-                            'The selected user id have no scheduled other admission test after than now.'
-                        );
-                    } elseif ($user->hasSamePassportAlreadyQualificationOfMembership) {
-                        $validator->errors()->add(
-                            'user_id',
-                            'The passport of selected user id has already been qualification for membership.'
-                        );
-                    } elseif ($user->lastAttendedAdmissionTestOfOtherSamePassportUser) {
-                        $validator->errors()->add(
-                            'user_id',
-                            'The selected user id has other same passport user account tested.'
-                        );
-                    } elseif (
-                        $user->lastAttendedAdmissionTest &&
-                        $user->lastAttendedAdmissionTest->testing_at
-                            ->addMonths(
-                                $user->lastAdmissionTest->type->interval_month
-                            )->endOfDay() >= $now
-                    ) {
-                        $validator->errors()->add(
-                            'user_id',
-                            "The selected user id has admission test record within {$user->lastAdmissionTest->type->interval_month} months(count from testing at of this test sub {$user->lastAdmissionTest->type->interval_month} months to now)."
-                        );
-                    } elseif (! $user->defaultEmail && ! $user->defaultMobile) {
-                        $validator->errors()->add(
-                            'user_id',
-                            'The selected user must at least has default contact.'
-                        );
-                    }
-                }
-            },
-        ];
     }
 }
