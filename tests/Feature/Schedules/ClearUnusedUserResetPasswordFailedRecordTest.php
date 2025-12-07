36a4e633f89cdaa08ea4e78de11a613df3ae7b9d
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-12-07 20:38:44 +0800

    let ClearUnusedUserResetPasswordFailedRecordTest code cleaning

--- tests/Feature/Schedules/ClearUnusedUserResetPasswordFailedRecordTest.php
+++ tests/Feature/Schedules/ClearUnusedUserResetPasswordFailedRecordTest.php
@@ -67,66 +67,43 @@ class ClearUnusedUserResetPasswordFailedRecordTest extends TestCase
 
     private function create_admin_reset_password_record_without_pass_24_hours()
     {
-        (new ResetPasswordLog)
-            ->fillable([
-                'passport_type_id',
-                'passport_number',
-                'contact_type',
-                'user_id',
-                'creator_id',
-                'creator_ip',
-                'middleware_should_count',
-                'created_at',
-            ])->insert([
-                'passport_type_id' => $this->user->passport_type_id,
-                'passport_number' => $this->user->passport_number,
-                'contact_type' => $this->contact->type,
-                'user_id' => $this->user->id,
-                'creator_id' => $this->user->id,
-                'creator_ip' => fake()->ipv4(),
-                'middleware_should_count' => false,
-                'created_at' => now()->subDay()->subSecond(),
-            ]);
+        $log = new ResetPasswordLog;
+        $log->timestamps = false;
+        $log->passport_type_id = $this->user->passport_type_id;
+        $log->passport_number = $this->user->passport_number;
+        $log->contact_type = $this->contact->type;
+        $log->user_id = $this->user->id;
+        $log->creator_id = $this->user->id;
+        $log->creator_ip = fake()->ipv4();
+        $log->middleware_should_count = false;
+        $log->created_at = now()->subDay()->subSecond();
+        $log->save();
     }
 
     private function create_success_user_reset_password_record_without_pass_24_hours()
     {
-        (new ResetPasswordLog)
-            ->fillable([
-                'passport_type_id',
-                'passport_number',
-                'contact_type',
-                'user_id',
-                'creator_id',
-                'creator_ip',
-                'created_at',
-            ])->insert([
-                'passport_type_id' => $this->user->passport_type_id,
-                'passport_number' => $this->user->passport_number,
-                'contact_type' => $this->contact->type,
-                'user_id' => $this->user->id,
-                'creator_id' => $this->user->id,
-                'creator_ip' => fake()->ipv4(),
-                'created_at' => now()->subDay()->subSecond(),
-            ]);
+        $log = new ResetPasswordLog;
+        $log->timestamps = false;
+        $log->passport_type_id = $this->user->passport_type_id;
+        $log->passport_number = $this->user->passport_number;
+        $log->contact_type = $this->contact->type;
+        $log->user_id = $this->user->id;
+        $log->creator_id = $this->user->id;
+        $log->creator_ip = fake()->ipv4();
+        $log->created_at = now()->subDay()->subSecond();
+        $log->save();
     }
 
     private function create_failed_user_reset_password_record_without_pass_24_hours()
     {
-        (new ResetPasswordLog)
-            ->fillable([
-                'passport_type_id',
-                'passport_number',
-                'contact_type',
-                'creator_ip',
-                'created_at',
-            ])->insert([
-                'passport_type_id' => $this->user->passport_type_id,
-                'passport_number' => $this->user->passport_number,
-                'contact_type' => $this->contact->type,
-                'creator_ip' => fake()->ipv4(),
-                'created_at' => now()->subDay()->subSecond(),
-            ]);
+        $log = new ResetPasswordLog;
+        $log->timestamps = false;
+        $log->passport_type_id = $this->user->passport_type_id;
+        $log->passport_number = $this->user->passport_number;
+        $log->contact_type = $this->contact->type;
+        $log->creator_ip = fake()->ipv4();
+        $log->created_at = now()->subDay()->subSecond();
+        $log->save();
     }
 
     public function test_only_has_admin_reset_password_record_within_pass_24_hours()
