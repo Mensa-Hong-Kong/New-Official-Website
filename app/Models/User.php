7907ccc3ec38d092d5bd58cd2f051d40ba278fd8
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2025-12-17 21:46:58 +0800

    add synced_to_stripe boolean cast to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,7 @@ class User extends Authenticatable
     protected $casts = [
         'password' => 'hashed',
         'birthday' => 'date',
+        'synced_to_stripe' => 'boolean',
     ];
 
     /**
