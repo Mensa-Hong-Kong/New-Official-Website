0b83f4320dc5512ffccbe906b99f8e0e98efcb87
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2026-02-03 12:28:41 +0800

    rename ModuleSeeder to ModulePermissionSeeder

--- database/migrations/2024_12_29_222606_run_module_permission_seeder.php
+++ database/migrations/2024_12_29_222606_run_module_permission_seeder.php
@@ -8,7 +8,7 @@ return new class extends Migration
     public function up(): void
     {
         Artisan::call('db:seed', [
-            '--class' => 'ModuleSeeder',
+            '--class' => 'ModulePermissionSeeder',
             '--force' => true,
         ]);
     }
