99d884ba50693fa79c1739b9bef891afc4ae0bcb
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2026-05-10 19:54:25 +0800

    fix admission test model hasUnusedQuota method test missing user_id

--- tests/Feature/Models/AdmissionTestOrder/HasUnusedQuotaTest.php
+++ tests/Feature/Models/AdmissionTestOrder/HasUnusedQuotaTest.php
@@ -4,11 +4,14 @@ namespace Tests\Feature\Models\AdmissionTestOrder;
 
 use App\Models\AdmissionTest;
 use App\Models\AdmissionTestOrder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class HasUnusedQuotaTest extends TestCase
 {
-    public function test_order_status_is_not_paid()
+    use RefreshDatabase;
+
+    public function test_order_status_is_not_paid(): void
     {
         $order = AdmissionTestOrder::factory()->create([
             'status' => fake()->randomElement(['pending', 'failed', 'cancelled', 'expired']),
@@ -17,7 +20,7 @@ class HasUnusedQuotaTest extends TestCase
         $this->assertFalse($order->has_unused_quota);
     }
 
-    public function test_returned_quota_plus_attended_tests_count_is_equal_or_more_than_quota()
+    public function test_returned_quota_plus_attended_tests_count_is_equal_or_more_than_quota(): void
     {
         $order = AdmissionTestOrder::factory()->state([
             'status' => 'succeeded',
@@ -38,7 +41,7 @@ class HasUnusedQuotaTest extends TestCase
         $this->assertFalse($order->has_unused_quota);
     }
 
-    public function test_quota_is_expired()
+    public function test_quota_is_expired(): void
     {
         $order = AdmissionTestOrder::factory()->state([
             'status' => 'succeeded',
@@ -50,7 +53,7 @@ class HasUnusedQuotaTest extends TestCase
         $this->assertFalse($order->has_unused_quota);
     }
 
-    public function test_order_status_is_paid_and_returned_quota_plus_attended_tests_count_is_less_than_quota_and_quota_is_not_expired()
+    public function test_order_status_is_paid_and_returned_quota_plus_attended_tests_count_is_less_than_quota_and_quota_is_not_expired(): void
     {
         $order = AdmissionTestOrder::factory()->state([
             'status' => 'succeeded',
@@ -59,11 +62,14 @@ class HasUnusedQuotaTest extends TestCase
             'quota_validity_months' => 1,
             'created_at' => now()->subDays(20),
         ])->create();
-
-        $order->tests()->attach(
-            $order->user_id, ['is_present' => true]
+        $test = AdmissionTest::factory()->create();
+        $test->candidates()->attach(
+            $order->user_id,
+            [
+                'order_id' => $order->id,
+                'is_present' => true,
+            ]
         );
-
         $this->assertTrue($order->has_unused_quota);
     }
 }
