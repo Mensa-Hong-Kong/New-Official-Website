c01656ed8ebcc5df18b5475d5da5b1249da38396
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2026-05-24 16:06:52 +0800

    limit super admin role only work in local or uat

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -21,8 +22,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::before(function ($user, $ability) {
-            return $user->hasRole('Super Administrator') ? true : null;
-        });
+        if (! in_array(config('app.env', 'production'), ['production', 'state'])) {
+            Gate::before(
+                function (User $user, string $ability) {
+                    return $user->hasRole('Super Administrator') ? true : null;
+                }
+            );
+        }
     }
 }
