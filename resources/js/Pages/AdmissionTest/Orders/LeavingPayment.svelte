80a6c91cab728aa7986413452f0aca8a05895277
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2026-05-27 14:08:04 +0800

    add leaving payment page for admission test order cancel url

---
+++ resources/js/Pages/AdmissionTest/Orders/LeavingPayment.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+    import { seo } from '@/Pages/Layouts/App.svelte';
+    import { Link, router } from "@inertiajs/svelte";
+
+    seo.title = 'Leaving Payment Page';
+
+	let count = 5; // Set the countdown duration in seconds
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			if (count > 1) {
+				count -= 1;
+			} else {
+				clearInterval(interval);
+				router.get('admission-tests.index');
+			}
+		}, 1000);
+
+		// Cleanup the interval when the component is destroyed
+		return () => clearInterval(interval);
+	});
+</script>
+
+<section class="container">
+    <h2 class="mb-2 fw-bold">Leaving Payment Page</h2>
+    <article>
+        <p>Your payment has not been completed yet. You will be redirected to the admission test index in <span>{count}</span> seconds.</p>
+        <p>If you aren't redirected automatically, <Link href={route('admission-tests.index')}>click here to return to the index</Link>.</p>
+    </article>
+</section>
