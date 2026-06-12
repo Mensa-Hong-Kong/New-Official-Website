f75d277b935c52d3852d0c89e31d43424b38ac84
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2026-06-12 19:47:36 +0800

    update StripeAlertCustomer component for add synced to stripe web socket

--- resources/js/Pages/Components/StripeAlert/Customer.svelte
+++ resources/js/Pages/Components/StripeAlert/Customer.svelte
@@ -16,11 +16,15 @@
             `${modelName}.${customer.id}`,
             [
                 '.StripeCustomerCreated',
+                '.SyncedToStripe',
                 '.DefaultEmail',
             ],
             (event) => {
                 if (event.created_stripe_customer !== undefined) {
                     customer.created_stripe_customer = event.created_stripe_customer;
+                }
+                if (event.synced_to_stripe !== undefined) {
+                    customer.synced_to_stripe = event.synced_to_stripe;
                 }
                 if (event.default_email !== undefined) {
                     customer.default_email = event.default_email;
@@ -38,6 +42,10 @@
     {:else if ! customer.default_email}
         <Alert color="danger">
             You have no default email, the stripe cannot send the receipt to you.
+        </Alert>
+    {:else if ! customer.synced_to_stripe}
+        <Alert color="danger">
+            We are syncing your information to stripe, please wait a few minutes, when synced, this alert will be close. You also can keep going to transaction but the receipt may not sent to your expected email or name not same with your profile.
         </Alert>
     {/if}
 {/if}
