a1b4db13f0da6aa162eb7aeb8fa17561228d4466
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2026-06-12 09:42:23 +0800

    fount end change from polling check stripe customer created to use web socket

--- resources/js/Pages/Components/StripeAlert/Customer.svelte
+++ resources/js/Pages/Components/StripeAlert/Customer.svelte
@@ -1,37 +1,35 @@
 <script>
     import { Alert } from '@sveltestrap/sveltestrap';
+    import { useEcho } from "@laravel/echo-svelte";
+
     let { customer = $bindable(), type } = $props();
 
-    if(! customer.created_stripe_customer) {
-        let routeName;
-        switch(type) {
-            case 'user':
-                routeName = "profile.created-stripe-customer";
-                break;
-        }
-        function checkCustomerCreated() {
-            axios.get(
-                route(routeName)
-            ).then(
-                function (response) {
-                    if(response.data.status) {
-                        customer.created_stripe_customer = true;
-                        clearInterval(checkCustomerCreatedInterval);
-                    }
-                }
-            );
-        }
+    let modelName;
 
-        let checkCustomerCreatedInterval = setInterval(checkCustomerCreated, 30000);
+    switch(type) {
+        case 'user':
+            modelName = `App.Models.User`;
+    }
+
+    if (modelName && customer.id) {
+        useEcho(
+            `${modelName}.${customer.id}`,
+            '.StripeCustomerCreated',
+            (event) => {
+                customer.created_stripe_customer = event.created_stripe_customer;
+            }
+        );
     }
 </script>
 
-{#if ! customer.created_stripe_customer}
-    <Alert color="danger">
-        We are creating your customer account on stripe, please wait a few minutes, when created, this alert will be close.
-    </Alert>
-{:else if ! customer.default_email}
-    <Alert color="danger">
-        You have no default email, the stripe cannot send the receipt to you. If you added default email, please reload this page to confirm the email has been synced to stripe.
-    </Alert>
+{#if modelName && customer.id}
+    {#if ! customer.created_stripe_customer}
+        <Alert color="danger">
+            We are creating your customer account on stripe, please wait a few minutes, when created, this alert will be close.
+        </Alert>
+    {:else if ! customer.default_email}
+        <Alert color="danger">
+            You have no default email, the stripe cannot send the receipt to you. If you added default email, please reload this page to confirm the email has been synced to stripe.
+        </Alert>
+    {/if}
 {/if}
