91d56b016f9517b3635c3cd4b703b91d309c8ac2
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2026-06-13 21:29:29 +0800

    add app/Library/Stripe/Jobs/SyncPriceToStripe

---
+++ app/Library/Stripe/Jobs/SyncPriceToStripe.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Library\Stripe\Jobs;
+
+use Illuminate\Support\Facades\DB;
+
+class SyncPriceToStripe extends Base
+{
+    public function handle(): void
+    {
+        DB::beginTransaction();
+        $model = $this->model::lockForUpdate()
+            ->find($this->modelID);
+        if (! $model->product->stripe_id) {
+            DB::rollBack();
+            $this->release(60);
+        } elseif (
+            (
+                in_array('stripe_one_time_type_id', $model->getFillable()) &&
+                ! $model->synced_one_time_type_to_stripe
+            ) || (
+                in_array('stripe_recurring_type_id', $model->getFillable()) &&
+                ! $model->synced_recurring_type_to_stripe
+            )
+        ) {
+            if (
+                in_array('stripe_one_time_type_id', $model->getFillable()) &&
+                ! $model->synced_one_time_type_to_stripe
+            ) {
+                $model->stripeUpdateOrCreate('one_time');
+            }
+            if (
+                in_array('stripe_recurring_type_id', $model->getFillable()) &&
+                ! $model->synced_recurring_type_to_stripe
+            ) {
+                $model->stripeUpdateOrCreate('recurring');
+            }
+            DB::commit();
+        } else {
+            DB::rollBack();
+        }
+    }
+}
