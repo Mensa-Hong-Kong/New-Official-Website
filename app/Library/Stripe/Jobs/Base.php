b64b8d7bc7bed5ff6bd28ebec5408c8f206d7e9d
黃梓榆 <tsz-yu.wong@mensa.org.hk>
2026-06-13 19:58:49 +0800

    add app/Library/Stripe/Jobs/Base

---
+++ app/Library/Stripe/Jobs/Base.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Library\Stripe\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Middleware\ThrottlesExceptions;
+use Illuminate\Queue\Middleware\WithoutOverlapping;
+
+abstract class Base implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable;
+
+    public function __construct(
+        protected string $model,
+        protected int $modelID
+    ) {}
+
+    public function middleware(): array
+    {
+        return [
+            new ThrottlesExceptions(10, 5 * 60),
+            new WithoutOverlapping($this->modelID),
+        ];
+    }
+
+    abstract public function handle(): void;
+}
